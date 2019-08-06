9d96db170d4f3616fad384123a79d2f9b1032c4b
HSR <hectorsilvarobles@gmail.com>
2019-08-06 16:35:54 -0700

    making a commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import clouds from './Media/cloud.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className='header'>
+        <div className='logo'>
+          <div className='logo-hsr'><h1>HSR</h1></div>
+          <div className='slogo'>
+            <h1>Hector Airlines</h1>
+            <h2>The Best Flights Around</h2>
+          </div>
+        </div>
+        <nav className='nav'>
+          <ul>
+            <li>Home</li>
+            <li>Flights</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
       </header>
+      <body className='banner'>
+        <div className='left-side'>
+          <div className='info'>
+              <h2>Are you ready for</h2>
+              <h1>Take-Off?</h1>
+              <p>Let's start Day 2 with a hero-banner of airlines website. I find ideas for User Interfaces and designs on Pinterest.</p>
+          </div>
+          <div className='buttons'>
+            <button>Learn More</button>
+            <button className='book-now'>Book Now</button>
+          </div>
+        </div>
+        <div className='visual-section'>
+            <img className='clouds' src={clouds} />
+        </div>
+      </body>
     </div>
   );
 }
